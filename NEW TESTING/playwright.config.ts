061b8d74d41114a3f34b6fb9c21203a4085b2b25
Priyeshchikhaliya <ppriyesh61@gmail.com>
2022-08-02 19:07:34 +0530

    Done

--- NEW TESTING/playwright.config.ts
+++ NEW TESTING/playwright.config.ts
@@ -1,6 +1,6 @@
 import type { PlaywrightTestConfig } from "@playwright/test";
 
 const config: PlaywrightTestConfig = {
-  
+  testMatch: ["tests/login.test.ts"]
 };
 export default config;
--- NEW TESTING/tests/login.test.ts
+++ NEW TESTING/tests/login.test.ts
@@ -1,9 +1,10 @@
-import { chromium } from "@playwright/test";
+import { webkit, test } from "@playwright/test";
 
 test("Login Test", async () => {
-  const browser = chromium.launch();
-  const context = (await browser).newContext();
-  const page = (await context).newPage();
+  const browser = await webkit.launch();
+  const context = await browser.newContext();
+  const page = await context.newPage();
+  await page.goto("https://letcode.in/");
   // Gracefully close up everything
   await context.close();
   await browser.close();
